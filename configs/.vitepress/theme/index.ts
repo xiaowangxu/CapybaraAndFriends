3a84b495ba6397ff9574068287529d144eaac89d
王徐 <2208740924@qq.com>
2026-09-04 16:15:46 +0800

    fix: 延迟加载 shader-doodle 以支持服务器端渲染

--- configs/.vitepress/theme/index.ts
+++ configs/.vitepress/theme/index.ts
@@ -2,11 +2,13 @@
 import Layout from './Layout.vue'
 import type { Theme } from 'vitepress'
 import './style.css'
-import 'shader-doodle'
 
 export default {
   Layout,
   enhanceApp({ app }) {
+    if (!import.meta.env.SSR) {
+      import('shader-doodle')
+    }
   }
 } satisfies Theme
 
